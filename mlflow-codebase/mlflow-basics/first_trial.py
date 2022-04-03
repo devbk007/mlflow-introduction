6862bf5361e149bd9784f43bfb2c54c602de6629
baizel <baizelkv@gmail.com>
2022-04-03 14:46:24 +0530

    mlflow-basics

--- mlflow-codebase/mlflow-basics/first_trial.py
+++ mlflow-codebase/mlflow-basics/first_trial.py
@@ -5,7 +5,7 @@ def calculate_nth_power(x, n):
 
 if __name__ == '__main__':
     with mlflow.start_run():
-        x, n = 4, 8
+        x, n = 2, 4
         y = calculate_nth_power(x, n)
         mlflow.log_param("x", x)
         mlflow.log_param("n", n)
